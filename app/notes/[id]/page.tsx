56ef1807c7e3ce32ccf2e00abb29ae2fca27af22
guolong <guolong.deng@hand-china.com>
2023-07-19 16:37:55 +0800

    feat: 完成根据 id 完成页面导航

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,28 @@
+import styles from '../Notes.module.css'
+
+const getNote = async (noteId: string) => {
+  const res = await fetch(
+    `http://127.0.0.1:8090/api/collections/next_demo/records/${noteId}`,
+  );
+  const data = await res.json();
+  return data;
+
+}
+
+
+const NotePage = async ({ params }: any) => {
+  const note = await getNote(params.id);
+
+
+  return (
+    <div>
+      <h1>notes/{note.id}</h1>
+      <div className={styles.note}>
+        <h2>{note.title}</h2>
+        <h5>{note.content}</h5>
+        <p>{note.created}</p>
+      </div>
+    </div>
+  )
+}
+export default NotePage
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,28 +1,29 @@
-// import PocketBase from 'pocketbase';
+import PocketBase from 'pocketbase';
 import Link from 'next/link';
-import styles from './Notes.module.css';
+import styles from './Notes.module.css'
 
-// export const dynamic = 'auto',
-//   dynamicParams = true,
-//   revalidate = 0,
-//   fetchCache = 'auto',
-//   runtime = 'nodejs',
-//   preferredRegion = 'auto'
+export const dynamic = 'auto',
+  dynamicParams = true,
+  revalidate = 0,
+  fetchCache = 'auto',
+  runtime = 'nodejs',
+  preferredRegion = 'auto'
 
 
 async function getNotes() {
-  // const db = new PocketBase('http://127.0.0.1:8090');
-  // const result = await db.records.getList('notes');
-  const res = await fetch('http://127.0.0.1:8090/api/collections/next_demo/records?page=1&perPage=30', { cache: 'no-store' });
-  const data = await res.json();
-  return data?.items as any[];
+  const db = new PocketBase('http://127.0.0.1:8090');
+  const data = await db.collection('next_demo').getFullList();
+  // const res = await fetch('http://127.0.0.1:8090/api/collections/next_demo/records?page=1&perPage=30', { cache: 'no-store' });
+  // const data = await res.json();
+  return data;
   
 }
 
 export default async function NotesPage() {
   const notes = await getNotes();
   console.log('1111111');
-  
+  console.log(notes);
+
   return(
     <div>
       <h1>Notes</h1>
